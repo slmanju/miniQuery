d41d42ee5520b60a613b6f6cafe8ca1366b384c7
Manjula Jayawardana <30832256+slmanju@users.noreply.github.com>
2017-09-06 14:38:48 +0530

    add basic functions
    each, text, on, addClass, removeClass functions are added

--- miniQuery.js
+++ miniQuery.js
@@ -16,8 +16,44 @@
     MiniQuery.fn = MiniQuery.prototype;
     window.MiniQuery = window.mj = MiniQuery;
 
-    MiniQuery.fn.text = function () {
+    // iterate over all the elements. calling context is element
+    MiniQuery.fn.each = function (callback) {
+        var i;
+        for (i = 0; i < this.length; i = i + 1) {
+            callback.call(this.nodes[i], this.nodes[i], i);
+        }
+        return this;
+    };
+
+    // add text value to all the elements or just return first element's value
+    MiniQuery.fn.text = function (textValue) {
+        if (textValue) {
+            return this.each(function () {
+                this.innerText = textValue;
+            });
+        }
         return this.nodes[0].innerText;
+    };
+
+    // add class to all elements
+    MiniQuery.fn.addClass = function (className) {
+        return this.each(function () {
+            this.className = this.className + " " + className;
+        });
+    };
+
+    // remove class from all elements
+    MiniQuery.fn.removeClass = function(className) {
+        return this.each(function() {
+            this.className = this.className.replace(new RegExp('\\b' + className + '\\b', 'g'), '');
+        });
+    };
+
+    // on action performed
+    MiniQuery.fn.on = function(name, handler) {
+        return this.each(function() {
+            this.addEventListener(name, handler, false);
+        });
     };
 
 }(window));
