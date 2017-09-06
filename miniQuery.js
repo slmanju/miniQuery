0bbe623569f0f5d58f006aa64fcf53c8bd1b710d
Manjula Jayawardana <30832256+slmanju@users.noreply.github.com>
2017-09-06 13:59:38 +0530

    select nodes by query selector and get text

--- miniQuery.js
+++ miniQuery.js
@@ -5,8 +5,19 @@
         if (!(this instanceof MiniQuery)) {
             return new MiniQuery(selector);
         }
+        this.length = 0; // Number of elements in collection
+        this.nodes = []; // Nodes collection array
+        this.nodes = document.querySelectorAll(selector);
+        if (this.nodes.length) {
+            this.length = this.nodes.length;
+        }
     }
 
     MiniQuery.fn = MiniQuery.prototype;
-    window.MiniQuery = window.mq = MiniQuery;
+    window.MiniQuery = window.mj = MiniQuery;
+
+    MiniQuery.fn.text = function () {
+        return this.nodes[0].innerText;
+    };
+
 }(window));
